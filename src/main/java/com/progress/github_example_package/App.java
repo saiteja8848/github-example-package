e2f47fd1523ec157e02f0ce50e8a955161392d07
saiteja8848 <saiteja8848@gmail.com>
2021-02-06 16:25:07 +0530

    first commit

---
+++ src/main/java/com/progress/github_example_package/App.java
@@ -0,0 +1,13 @@
+package com.progress.github_example_package;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
